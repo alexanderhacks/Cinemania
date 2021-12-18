8c190d7331c4188c0204e2a0a468edaadb12fc23
Alexander Morales Panitz <alexander.morales@utec.edu.pe>
2021-12-17 19:39:50 -0500

    welcome

--- main.cpp
+++ main.cpp
@@ -3,42 +3,10 @@
 
 using namespace std;
 
-class Printer {
-  string _name;
-  int _availablePaper;
-public:
-  Printer() = default;
-  Printer(string name, int paper) {
-    _name = name;
-    _availablePaper = paper;
-  }
-  void Print(string txtDoc) {
-
-    int requiredPaper = txtDoc.length()/10; // 40/10 = 4
-
-    if(requiredPaper>_availablePaper)
-    {
-      throw "No paper available.";
-    }
-
-    cout << "Printing..." << txtDoc << endl;
-    _availablePaper -= requiredPaper;
-  }
-};
-
 int main()
 {
-  Printer myPrinter("HP Desktop1234", 10);
-  try
-  {
-    myPrinter.Print("Hello, my name is Alexander. I'm a Software Engineer");
-    myPrinter.Print("Hello, my name is Alexander. I'm a Software Engineer");
-    myPrinter.Print("Hello, my name is Alexander. I'm a Software Engineer");
-    myPrinter.Print("Hello, my name is Alexander. I'm a Software Engineer");
-  }
-  catch (const char* txtException)
-  {
-    cout << "Exception: " << txtException << endl;
-  }
+  // Mensaje de entrada
+  cout << "¡Bienvenido a Cinemania! Tu mejor experiencia en el cine, aquí.\n";
+
   return 0;
 }
