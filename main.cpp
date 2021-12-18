8c4921dc4864d76b64b6ed1415c0ee84c0a94eec
Alexander Morales Panitz <alexander.morales@utec.edu.pe>
2021-12-17 19:44:59 -0500

    Colors adding

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <string>
+#include "lib/colormod.h"
 
 using namespace std;
 
 int main()
 {
+  // Importamos los namespace heredados de la clase colormod para el manejo de colores.
+  Color::Modifier red(Color::FG_RED);
+  Color::Modifier def(Color::FG_DEFAULT);
+  Color::Modifier green(Color::FG_GREEN);
   // Mensaje de entrada
-  cout << "¡Bienvenido a Cinemania! Tu mejor experiencia en el cine, aquí.\n";
+  cout << red << "¡Bienvenido a Cinemania!" << def << " Tu mejor experiencia en el cine, " << green << "aquí.\n";
 
   return 0;
 }
